1f33e0c94a9c1a40cfa1499618378d13066fa89d
Mehmet <104369440+mehmetzerey@users.noreply.github.com>
2025-03-12 18:01:22 +0300

    VehicleType enum'ı oluşturuldu

---
+++ src/OtoServisYonetim.Domain/Enums/VehicleType.cs
@@ -0,0 +1,57 @@
+namespace OtoServisYonetim.Domain.Enums;
+
+/// <summary>
+/// Araç tiplerini tanımlar
+/// </summary>
+public enum VehicleType
+{
+    /// <summary>
+    /// Sedan tipi araç
+    /// </summary>
+    Sedan = 1,
+    
+    /// <summary>
+    /// SUV tipi araç
+    /// </summary>
+    SUV = 2,
+    
+    /// <summary>
+    /// Hatchback tipi araç
+    /// </summary>
+    Hatchback = 3,
+    
+    /// <summary>
+    /// Station Wagon tipi araç
+    /// </summary>
+    StationWagon = 4,
+    
+    /// <summary>
+    /// Coupe tipi araç
+    /// </summary>
+    Coupe = 5,
+    
+    /// <summary>
+    /// Convertible tipi araç
+    /// </summary>
+    Convertible = 6,
+    
+    /// <summary>
+    /// Pickup tipi araç
+    /// </summary>
+    Pickup = 7,
+    
+    /// <summary>
+    /// Van tipi araç
+    /// </summary>
+    Van = 8,
+    
+    /// <summary>
+    /// Minivan tipi araç
+    /// </summary>
+    Minivan = 9,
+    
+    /// <summary>
+    /// Crossover tipi araç
+    /// </summary>
+    Crossover = 10
+}
